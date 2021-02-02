0f3da54404e716a7ee22d11cfbd421753957d0bc
陈晓铜 <1281148140@qq.com>
2021-02-02 12:09:53 +0800

    添加ui组件

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
-
+// main.js
+import uView from "uview-ui";
+Vue.use(uView);
 App.mpType = 'app'
 
 const app = new Vue({

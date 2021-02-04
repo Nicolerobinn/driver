4a7fd4eb57cc52d30ed8f9fc170513af2b312d31
陈晓铜 <1281148140@qq.com>
2021-02-04 17:38:18 +0800

    新增需求页面

---
+++ pages/personal/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="content">
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
